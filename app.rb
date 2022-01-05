231f0badc2e18b4a8523748bf103edf1378f18ef
Cory <cameyer182@gmail.com>
2022-01-05 14:38:34 -0500

    commit tree 90fa7c7f5398ee800684fa0315577f24b78e3f49

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/' do
+        erb :results
+    end
+
+    post '/piglatinize' do
+        @analyzed_text = PigLatinizer.new
+        erb :piglatinize
+    end
 end
